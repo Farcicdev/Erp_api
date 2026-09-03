6d647821749d690fd5ccae697352deec880b60fd
FarcicDev <augustofarcic@gmail.com>
2026-09-03 11:31:34 -0300

    first commit

---
+++ src/main/java/farcic/dev/erp_gestao/ErpGestaoApplication.java
@@ -0,0 +1,13 @@
+package farcic.dev.erp_gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpGestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpGestaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/farcic/dev/erp_gestao/ErpGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package farcic.dev.erp_gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
