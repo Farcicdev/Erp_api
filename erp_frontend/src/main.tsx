fdce7e71e07179619d7bb511af1414481eaee7ed
FarcicDev <augustofarcic@gmail.com>
2026-09-17 13:11:23 -0300

    config keycloak frontend

--- erp_frontend/src/main.tsx
+++ erp_frontend/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import keycloak from "./auth/keycloak.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+keycloak.init({
+    onLoad: 'check-sso',
+    pkceMethod: 'S256',
+}).then(() => {
+    createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    )
+}).catch((erro) => {
+    console.error('Erro ao inicializar o Keycloak', erro)
+})
